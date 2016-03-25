a61f19e42b1c753a8d0712c854338c08aab53d6e
Andobas <one.e.toe@gmail.com>
2016-03-24 20:42:11 -0700

    Working on Custom View, Step 4

---
+++ TinderLab/DraggableImageView.swift
@@ -0,0 +1,21 @@
+//
+//  DraggableImageView.swift
+//  TinderLab
+//
+//  Created by Juan Hernandez on 3/24/16.
+//  Copyright © 2016 ccsf. All rights reserved.
+//
+
+import UIKit
+
+class DraggableImageView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
